ab34c14435d54c6cf8939a1200bae27592e3951e
Mte90 <mte90smanettator@gmail.com>
2012-08-08 00:32:49 +0200

    first invoice date

--- save.php
+++ save.php
@@ -2,8 +2,7 @@
 	include('./config.php');
 
 	if($_GET['mode']='save_invoice') {
-		$content  = $_GET['content'];
-		$content .= '<invoice>'."\n\t";
+		$content = '<invoice>'."\n\t";
 		$content .= '<number>'.clean($_GET['invoice_number']).'</number>'."\n\t";
 		$content .= '<ticket>'.clean($_GET['invoice_ticket']).'</ticket>'."\n\t";
 		$content .= '<note>"'.clean($_GET['note']).'"</note>'."\n\t";
@@ -11,6 +10,8 @@
 		$content .= '<tax>'.clean($_GET['tax']).'</tax>'."\n\t";
 		$content .= '<address>"'.clean($_GET['address']).'"</address>'."\n";
 		$content .= '</invoice>';
+		$content .= $_GET['content'];
+
 		file_put_contents('./invoice/'.clean($_GET['invoice_number']).'.xml',$content);
 	}
 
