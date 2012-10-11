8dd65e202f06a7f62de4700e954faeb99262aaff
Mte90 <mte90smanettator@gmail.com>
2012-10-11 16:21:02 +0200

    fixed code

--- note_data.php
+++ note_data.php
@@ -1,6 +1,6 @@
 <?
 	require_once('./config.php');
 
-	$draft_data = read_note_info($_GET['number']);
-	echo json_encode($draft_data);
+	$note_data = read_note_info($_GET['number']);
+	echo json_encode($note_data);
 ?>
