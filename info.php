b20958a83b3c449ffce2e782eea18b009cfd08c8
Mte90 <mte90smanettator@gmail.com>
2013-02-19 15:53:08 +0100

    added settings

--- info.php
+++ info.php
@@ -10,12 +10,9 @@
 		<link type="text/css" rel="stylesheet" href="css/bootstrap.min.css">
 	</head>
 	<body>
-		<h2 style="margin:0 auto;width:550px"><? echo $l10n['TITLE'].' - '.$config['version']; ?></h2><br>
+		<div style="margin:0 auto;width:550px"><h2><? echo $l10n['TITLE'].' - '.$config['version']; ?></h2>
+		<h4>Base Settings</h4></div>
 		<table class="table table-striped table-bordered" style="width:40%;margin:0 auto">
-			<tr>
-				<th>Base Settings</th>
-				<th>Value</th>
-			</tr>
 			<tr>
 				<td>Language</td>
 				<td><? echo $config['language']; ?></td>
@@ -31,6 +28,45 @@
 			<tr>
 				<td>Default Logo</td>
 				<td><img src="<? echo $path['logos'].DIRECTORY_SEPARATOR.$config['default_logo']; ?>" /></td>
+			</tr>
+		</table>
+		<div style="margin:0 auto;width:550px"><h4>Settings</h4></div>
+		<table class="table table-striped table-bordered" style="width:40%;margin:0 auto">
+			<tr>
+				<th>Settings</th>
+				<th>Value</th>
+			</tr>
+			<tr>
+				<td>Insert the Ticket Number</td>
+				<td><? echo $config['ticket_number']? 'true' : 'false'; ?></td>
+			</tr>
+			<tr>
+				<td>Capture Payment</td>
+				<td><? echo $config['capture_payment']? 'true' : 'false'; ?></td>
+			</tr>
+			<tr>
+				<td>Insert the Logo in the Invoice</td>
+				<td><? echo $config['invoice_logo']? 'true' : 'false'; ?></td>
+			</tr>
+			<tr>
+				<td>Dot or Comma</td>
+				<td><? echo $config['dot_or_comma']? 'true' : 'false'; ?></td>
+			</tr>
+			<tr>
+				<td>Insert the year in the Invoice Number</td>
+				<td><? echo $config['capture_payment']? 'true' : 'false'; ?></td>
+			</tr>
+			<tr>
+				<td>Login</td>
+				<td><? echo $config['pdf']['enable']? 'true' : 'false'; ?></td>
+			</tr>
+			<tr>
+				<td>Backend</td>
+				<td><? echo $config['backend']; ?></td>
+			</tr>
+			<tr>
+				<td><b>Enable Debug</b></td>
+				<td><? echo $config['debug']? 'true' : 'false'; ?></td>
 			</tr>
 		</table>
 	</body>
